660c8d6a03415d25c56b362a9456138c9fe891a3
Daniel Collins <danielc111@gmail.com>
2019-01-21 23:42:18 -0500

    setup sample random note generator

--- PortableEarTrainer/ViewController.swift
+++ PortableEarTrainer/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
-
+    static let PLAY_RATE = 2.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        executeSample()
     }
 
-
+    func executeSample() {
+        let instr = AKRhodesPiano()
+        let delay = AKDelay(instr)
+        delay.time = 1.5 / ViewController.PLAY_RATE
+        delay.dryWetMix = 0.3
+        delay.feedback = 0.2
+        
+        let reverb = AKReverb(delay)
+        
+        let scale = [0, 2, 4, 5, 7, 9, 11, 12]
+        let performance = AKPeriodicFunction(frequency: ViewController.PLAY_RATE) {
+            var note = scale.randomElement()
+            let octave = [2, 3, 4, 5].randomElement()! * 12
+            if random(in: 0...10) < 1.0 { note! += 1 }
+            if !scale.contains(note! % 12) { print("ACCIDENT!") }
+            
+            let frequency = (note! + octave).midiNoteToFrequency()
+            
+            if random(in: 0...6) > 1.0 {
+                instr.trigger(frequency: frequency)
+            }
+        }
+        
+        AudioKit.output = reverb
+        do {
+            
+            try AudioKit.start(withPeriodicFunctions: performance)
+        } catch {
+            print ("could not start audio")
+        }
+        performance.start()
+    }
 }
 
