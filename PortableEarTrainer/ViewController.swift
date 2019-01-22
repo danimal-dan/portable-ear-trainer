1f186d6d09fb27e8cfa96630042d376db479fe5c
Daniel Collins <danielc111@gmail.com>
2019-01-22 00:47:39 -0500

    Add another sample to play with

--- PortableEarTrainer/ViewController.swift
+++ PortableEarTrainer/ViewController.swift
@@ -11,11 +11,13 @@ import AudioKit
 
 class ViewController: UIViewController {
     static let PLAY_RATE = 2.0
+    static let MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11, 12]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        executeSample()
+//        executeSequence()
+        pulseCMajor()
     }
 
     func executeSample() {
@@ -27,12 +29,11 @@ class ViewController: UIViewController {
         
         let reverb = AKReverb(delay)
         
-        let scale = [0, 2, 4, 5, 7, 9, 11, 12]
         let performance = AKPeriodicFunction(frequency: ViewController.PLAY_RATE) {
-            var note = scale.randomElement()
+            var note = ViewController.MAJOR_SCALE.randomElement()
             let octave = [2, 3, 4, 5].randomElement()! * 12
             if random(in: 0...10) < 1.0 { note! += 1 }
-            if !scale.contains(note! % 12) { print("ACCIDENT!") }
+            if !ViewController.MAJOR_SCALE.contains(note! % 12) { print("ACCIDENT!") }
             
             let frequency = (note! + octave).midiNoteToFrequency()
             
@@ -44,6 +45,32 @@ class ViewController: UIViewController {
         AudioKit.output = reverb
         do {
             
+            try AudioKit.start(withPeriodicFunctions: performance)
+        } catch {
+            print ("could not start audio")
+        }
+        performance.start()
+    }
+    
+    func pulseCMajor() {
+        let instr = AKRhodesPiano()
+        let instr2 = AKRhodesPiano()
+        let instr3 = AKRhodesPiano()
+        
+        let C = (ViewController.MAJOR_SCALE[0] + 48).midiNoteToFrequency()
+        let E = (ViewController.MAJOR_SCALE[2] + 48).midiNoteToFrequency()
+        let G = (ViewController.MAJOR_SCALE[4] + 48).midiNoteToFrequency()
+        
+        let performance = AKPeriodicFunction(frequency: ViewController.PLAY_RATE) {
+            instr.trigger(frequency: C)
+            instr2.trigger(frequency: E)
+            instr3.trigger(frequency: G)
+        }
+        
+        let mixer = AKMixer(instr, instr2)
+        AudioKit.output = mixer
+        
+        do {
             try AudioKit.start(withPeriodicFunctions: performance)
         } catch {
             print ("could not start audio")
