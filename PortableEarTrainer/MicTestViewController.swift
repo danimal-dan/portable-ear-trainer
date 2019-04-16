d28d14a4267b0e7dfe9596f872a4dc803b428372
Daniel Collins <danielc111@gmail.com>
2019-04-15 23:42:37 -0400

    Add simple algorithm to detect note when background noise is present.

--- PortableEarTrainer/MicTestViewController.swift
+++ PortableEarTrainer/MicTestViewController.swift
@@ -14,6 +14,13 @@ class MicTestViewController: AKLiveViewController {
     var mic: AKMicrophone!;
     var tracker: AKFrequencyTracker!;
     var silence: AKBooster!;
+    var noiseFloor : Double = 0.01;
+    var frequencyHistory : [FrequencySnapshot] = [];
+    
+    struct FrequencySnapshot {
+        var frequency : Double;
+        var amplitude : Double;
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,10 +52,58 @@ class MicTestViewController: AKLiveViewController {
     }
     
     @objc func updateUI() {
-        if (tracker.amplitude > 0.1) {
+        if (tracker.amplitude > self.noiseFloor) {
+            let snapshot = FrequencySnapshot(frequency: tracker.frequency, amplitude: tracker.amplitude);
+            self.frequencyHistory.append(snapshot);
+            self.analyzeHistory();
             // TODO: should require user to maintain note for some number of frames to make sure choice is deliberate.
-            print("FREQUENCY: " + String(format: "%0.1f", tracker.frequency));
+            print("FREQUENCY: " + String(format: "%0.1f", snapshot.frequency));
+            print("AMPLITUDE: " + String(format: "%0.1f", snapshot.amplitude));
             print("NOTE NAME: " + NoteName.forFrequency(tracker.frequency))
+        }
+    }
+    
+    func analyzeHistory() {
+        if (self.frequencyHistory.count < 10) {
+            return;
+        }
+        
+        let frequencyList = self.frequencyHistory.map({ snapshot in
+            return snapshot.frequency;
+        });
+        
+        let amplitudeList = self.frequencyHistory.map({ snapshot in
+            return snapshot.amplitude;
+        });
+        
+        let frequencyStdDev = Stats.standardDeviation(frequencyList);
+        
+        if (frequencyStdDev < 15) {
+            print("note is steady");
+            print("---------");
+            print("NOTE SELECTED: " + NoteName.forFrequency(Stats.average(frequencyList)))
+            print("---------");
+        }
+        
+        if (frequencyStdDev > 100) {
+            print("frequency is sporadic")
+            let avgAmplitude = Stats.average(amplitudeList);
+            if (avgAmplitude < 0.4) {
+                print("average amplitude is low");
+                
+                let amplitudeStdDeviation = Stats.standardDeviation(amplitudeList);
+                if (amplitudeStdDeviation < 0.2) {
+                    print("amplitude is steady");
+                    print("raising noise floor")
+                    self.noiseFloor += 0.02;
+                    self.frequencyHistory = [];
+                }
+            }
+        }
+        
+        if (self.frequencyHistory.count > 20) {
+            print("reset history");
+            self.frequencyHistory = [];
         }
     }
 
